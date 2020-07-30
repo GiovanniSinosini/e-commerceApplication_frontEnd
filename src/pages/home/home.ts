cf0be8095c3f8fe9ef1a8652b64bba4abca7e8b8
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-07-30 18:44:41 +0100

    Home page => disable menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -8,7 +9,15 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
+  }
+
+  public ionViewWillEnter(){
+    this.menu.swipeEnable(false)
+  }
+
+  public ionViewDidLeave(){
+    this.menu.swipeEnable(true)
   }
 
   public login(){
