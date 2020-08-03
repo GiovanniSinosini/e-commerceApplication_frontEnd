0a9a7005760ddf10482fb6e7d77d3cf80d0e5c7a
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-08-03 21:31:21 +0100

    Capturing login data

---
+++ src/models/credentialsDTO.ts
@@ -0,0 +1,6 @@
+export interface CredentialsDTO{
+
+    email : string,
+    password : string
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { CredentialsDTO } from '../../models/credentialsDTO';
 
 @IonicPage()
 @Component({
@@ -8,6 +9,11 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds: CredentialsDTO = {
+    email : "",
+    password : ""
+  };
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
   }
@@ -21,6 +27,7 @@ export class HomePage {
   }
 
   public login(){
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriesPage')
   }
 
