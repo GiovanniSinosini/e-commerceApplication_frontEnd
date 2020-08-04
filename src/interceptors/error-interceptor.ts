43b72cd700314b5cd27386aee7d9053d5b26a7c8
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-08-04 19:52:25 +0100

    Profile page created

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { Observable } from 'rxjs/Rx'; 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
