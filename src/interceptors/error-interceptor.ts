6b7f881efc0268f870824e1c184373f9cb2e949d
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-08-05 22:12:03 +0100

    Handling other errors with alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; 
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -25,21 +26,55 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch(errorObj.status){
+
+                case 401:
+                    this.handle401();
+                    break;
+                
                 case 403: 
-                this.handle403();
-                break;
+                    this.handle403();
+                    break;
+                
+                default:
+                    this.handleDefaultError(errorObj);
+                
             }
 
             return Observable.throw(errorObj);
         }) as any;
     }
 
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: '401 Error: Authentication failure',
+            message: 'Incorrect email or password',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403(){
         this.storage.setLocalUser(null);
     }
 
-
-
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Error ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
 }
 
 export const ErrorInterceptorProvider = {
