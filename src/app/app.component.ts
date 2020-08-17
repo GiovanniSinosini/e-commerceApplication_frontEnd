26eac06be63ae6489128e85cad348aafd15f72dc
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-08-17 15:13:33 +0100

    ShoppingCart => floating buttons and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categories', component: 'CategoriesPage' },
+      { title: 'Shopping Basket', component: 'CartPage'},
       { title: 'Logout', component: ''}
     ];
 
